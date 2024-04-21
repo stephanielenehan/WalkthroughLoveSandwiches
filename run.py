89fd1b9fe4f742307e0e931ee1c9fc394f723c4a
Stephanie Lenehan <stephanie.lenehan@gmail.com>
2024-04-21 20:14:27 +0000

    Add requirements for deployment

--- run.py
+++ run.py
@@ -19,7 +19,7 @@ def get_sales_data():
     print("Data should be six numbers, separated by commas.")
     print("Example: 10,20,30,40,50,60/n")
 
-    data_str = input("Enter your data here: ")
+    data_str = input("Enter your data here:\n")
     print(f"The data provided is: {data_str}")
 
 def get_sales_data():
